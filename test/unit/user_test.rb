a616953d4abde9a81bec7808b3888bdb47e4e232
T.Barry <tobiebarry@hotmail.com>
2013-05-22 03:41:32 -0400

    added validation tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter a first name " do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+  
+  test "a user should enter a last name " do
+      user = User.new
+      assert !user.save
+      assert !user.errors[:last_name].empty?
+    end
+    
+  test "a user should enter a profile name" do
+      user = User.new
+      assert !user.save
+      assert !user.errors[:profile_name].empty?
+    end
+  
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = users(:asdf).profile_name
+    assert !user.save
+    puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end
+  
+  test "a user should have a proper profile name" do
+    user = User.new
+    user.profile_name = "profile name!"
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("is not the correct format")
+  end
+  
+  
 end
