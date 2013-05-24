1f83ccadd1bd2993d8708d769988a2d7d758d731
T.Barry <tobiebarry@hotmail.com>
2013-05-23 23:29:03 -0400

    added status validations and tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -11,17 +11,29 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should get new session" do
+  test "should be redirected when not logged in" do
     get :new
     assert_response :redirect
     assert_redirected_to new_user_session_path
   end
+  
+  test "should render the new page when logged in" do
+    sign_in users(:asdf)
+    get :new
+    assert_response :success
+  end
+  
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello world"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 
-  test "should create status" do
+  test "should create status when logged in" do
+    sign_in users(:asdf)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
-
     assert_redirected_to status_path(assigns(:status))
   end
 
@@ -30,21 +42,23 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    sign_in users(:asdf)
     get :edit, id: @status
     assert_response :success
-  end
+  end 
 
-  test "should update status" do
+  test "should update status when logged in" do
+    sign_in users(:asdf)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
-  test "should destroy status" do
+  test "should destroy status when logged in" do
+    sign_in users(:asdf)
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
     end
-
     assert_redirected_to statuses_path
   end
 end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a status requires content" do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+  
+  test "status's content is a least 2 letters" do
+    status = Status.new
+    status.content = 'h'
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+  
+  test "status has user id" do
+    status = Status.new
+    status.content = "hello world"
+    assert !status.save
+    assert !status.errors[:user_id].empty?
+  end
 end
